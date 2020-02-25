c0ea5dbaa5a6ce30198c09fdd20b15987dfd6da4
Gareevrm <Gareevrm@inbox.ru>
2020-02-26 00:22:50 +0300

    Lesson_2

---
+++ Lesson_2/kursjava/src/ru/ruslan/Main.java
@@ -0,0 +1,87 @@
+package ru.ruslan;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+
+        /*while (true){
+            double a = sc.nextDouble();
+            String oper = sc1.nextLine();
+            double b = sc.nextDouble();
+
+            switch (oper){
+                case "+":
+                    System.out.println(a + b);
+                    break;
+
+                case "-":
+                    System.out.println(a - b);
+                    break;
+
+                case "/":
+                    System.out.println(a / b);
+                    break;
+
+                case "*":
+                    System.out.println(a * b);
+                    break;
+
+
+
+            }
+
+            String [] fruits = {"Яблоко", "Груша", "Банан", "клубника"};
+            String pram = "";
+            for (int i=0;i<fruits.length;i++){
+                 pram = pram + fruits[i];
+            }
+            System.out.println(pram);
+
+            String [] param = new String[5];
+
+            for(int  i =0; i<param.length ; i++){
+                param[i] = sc.nextLine();
+            }
+
+            for (int i =0; i < param.length;i++){
+               System.out.println(param[i]);
+            }
+        }*/
+
+        int [] param = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int [] param1 = new int [10];
+        int [] param2 = new int [10];
+        //String [] param1 = new String [5];
+        //int count = 0;
+
+        for (int i=0; i<param.length; i++) {
+
+            if (param[i] % 2 == 0) {
+                param1[i] = param[i];
+                System.out.print(param1[i] + " ");
+            } else if (param[i] % 3 == 0) {
+                param2[i] = param[i];
+                System.out.println(param2[i] + " ");
+            }
+
+
+        }
+        /*for (int i = 0 ; i < param.length; i ++){
+            for (int j=0; j<param1.length;j++){
+                System.out.println(param[i] + " " + param1[j]);
+                if (param[i].equals(param1[j])){
+                    count ++;
+                }
+            }
+        }*/
+        //System.out.println(count);
+
+        // У тебя ьььь три каааа
+    }
+}
+
